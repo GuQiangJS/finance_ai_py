e0f796da6eb665b2383a346ab35cee97cb1d0f1d
GuQiangJS <guqiangjs@hotmail.com>
2018-12-23 00:19:35 +0800

    test.ipynb

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,11 +32,11 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.imgmath']
+              'sphinx.ext.doctest',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -82,7 +82,6 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -113,12 +112,10 @@ html_sidebars = {
     ]
 }
 
-
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'ORCAdoc'
-
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -148,7 +145,6 @@ latex_documents = [
      'GuQiangJS', 'manual'),
 ]
 
-
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
@@ -157,7 +153,6 @@ man_pages = [
     (master_doc, 'orca', 'ORCA Documentation',
      [author], 1)
 ]
-
 
 # -- Options for Texinfo output -------------------------------------------
 
@@ -169,9 +164,6 @@ texinfo_documents = [
      author, 'ORCA', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,2 @@
 # Copyright (C) 2018 GuQiangJs.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-
-
-from ._version import get_versions
-
-__version__ = get_versions()['version']
-del get_versions
